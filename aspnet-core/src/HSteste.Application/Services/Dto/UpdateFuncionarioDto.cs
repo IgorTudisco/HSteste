6d482f9e9fa5c39bfce1c40fbc958d3e8b8c558f
Igor Tudisco <igortudisco@hotmail.com>
2025-02-22 20:39:11 -0300

    Fix URL typo in NSwag configuration and update UpdateFuncionarioDto to remove inheritance and add nullable Id property

--- aspnet-core/src/HSteste.Application/Services/Dto/UpdateFuncionarioDto.cs
+++ aspnet-core/src/HSteste.Application/Services/Dto/UpdateFuncionarioDto.cs
@@ -2,8 +2,9 @@ using Abp.Application.Services.Dto;
 
 namespace HSteste.Application.Services.Dto
 {
-  public class UpdateFuncionarioDto : EntityDto<int>
+  public class UpdateFuncionarioDto
   {
+    public int? Id { get; set; }
     public string? Nome { get; set; }
     public string? Cpf { get; set; }
     public string? Email { get; set; }
